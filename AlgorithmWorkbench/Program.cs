c1b5ab610b124bfc47036fc36b95d6b574dbff68
adamkrieger <adamkrieger.ca@gmail.com>
2015-08-12 14:56:59 -0500

    Implemented spare change algorithm.

---
+++ AlgorithmWorkbench/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using LogExec;
+
+namespace AlgorithmWorkbench
+{
+	class MainClass
+	{
+		
+		public static void Main (string[] args)
+		{
+			var spareChange = new SpareChange ();
+
+			using (new ExecutionTimeLogger ("Code")) {
+				spareChange.Execute ();
+			}
+
+			Console.ReadLine ();
+		}
+	}
+}
---
+++ AlgorithmWorkbench/SpareChange.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgorithmWorkbench
+{
+	public class SpareChange
+	{
+		//This list must be sorted smallest to largest, contain no duplicates, and contain nothing less than 1
+		int[] _coins = {1,5,10,25,100};
+		const int _amount = 1000;
+
+		public SpareChange ()
+		{
+		}
+
+		public void Execute(){
+			int results = MakeChange (_amount, _coins);
+			Console.WriteLine(String.Format ("There are {0} ways to make change.", results)); 
+		}
+
+		private int MakeChange(int amount, int[] coinOptions){
+			int solutions = 0;
+
+			//For each coin in the coin options
+			for(var i = (coinOptions.Length-1
+			); i>=0; i--){
+				int remainder = amount - coinOptions [i];
+
+			//	If amount - value == 0
+			//		You've found an option, add one
+				if (remainder == 0) {
+					solutions++;
+				}
+
+			//	If amount - value < 0
+			//		You cannot make change with this coin, do not add
+				else if (remainder < 0) {
+					continue;
+				}
+			//	Otherwise
+			//		Find the number of MakeChange options if any coin larger is not an option
+				else {
+					solutions += MakeChange (remainder, coinOptions.Take (i + 1).ToArray ());
+				}
+			}
+
+			return solutions;
+		}
+	}
+}
+
